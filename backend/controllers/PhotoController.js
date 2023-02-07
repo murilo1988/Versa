0fafe1020c15719aa0d5a3e91d83f5b2fcdc1e5e
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 14:25:16 -0300

    inser photo was created

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,3 +1,17 @@
 const Photo = require("../models/Photo");
 
 const mongoose = require("mongoose");
+
+//Insert a photo , with user related to it
+
+const insertPhoto = async (req, res) => {
+  const { title } = req.body;
+  const image = req.file.filename;
+
+  console.log(req.body);
+  res.send("Foto inserida com sucesso");
+};
+
+module.exports = {
+  insertPhoto,
+};
