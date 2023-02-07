525a7488d2605a8bdd7c5bf4a8f9b6dd4434e8da
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 14:01:06 -0300

    middlewares was imported to create the routes

---
+++ backend/routes/PhotoRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+// controller
+
+// middlewares
+
+const { photoInsertValidation } = require("../middlewares/photoValidation");
+const authGuard = require("../middlewares/authGuard");
+const validate = require("../middlewares/handleValidation");
+// Routes
+
+module.exports = router;
