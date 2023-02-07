c466148556e1e4b2f96b3903a5872f25916e3ea7
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 16:00:37 -0300

    delete photos was  created

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -25,11 +25,43 @@ const insertPhoto = async (req, res) => {
     res
       .status(422)
       .json({ errors: ["Houve algum erro tente novamente mais tarde."] });
+    return;
   }
 
   res.status(201).json(newPhoto);
 };
 
+//remove a photo from DB
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+    // Check if photo exists
+    if (!photo) {
+      res.status(404).json("Foto não encontrada.");
+      return;
+    }
+    //check if photo belong to user
+    if (!photo.userId.equals(reqUser._id)) {
+      res.status(422).json({
+        errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
+      });
+    }
+    await Photo.findByIdAndDelete(photo._id);
+
+    res
+      .status(200)
+      .json({ id: photo._id, message: "Foto excluída com sucesso." });
+  } catch (error) {
+    res.status(404).json("Foto não encontrada.");
+    return;
+  }
+};
+
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
