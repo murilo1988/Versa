bbe788c7d8347c788e76da197bff2db36926de59
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 13:59:24 -0300

    photo insert validation was created

---
+++ backend/middlewares/photoValidation.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator");
+
+const photoInsertValidation = () => {
+  return [
+    body("title")
+      .not()
+      .equals("undefined")
+      .withMessage("O título é obrigatório.")
+      .isString()
+      .withMessage("o título é obrigatório.")
+      .isLength({ min: 3 })
+      .withMessage("O título precisa de no mínimo 3 caracteres."),
+    body("image").customValidator((value, { req }) => {
+      if (!req.file) {
+        throw new Error("A imagem é obrigatória.");
+      }
+      return true;
+    }),
+  ];
+};
+
+module.exports = { photoInsertValidation };
