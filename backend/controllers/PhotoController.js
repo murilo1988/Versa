0549a06d0e75d1a2b23f50892aa918e9aef2dc90
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 14:51:56 -0300

    created new photo with Photo.create  and verify if was created successfully

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,4 +1,5 @@
 const Photo = require("../models/Photo");
+const User = require("../models/User");
 
 const mongoose = require("mongoose");
 
@@ -8,8 +9,25 @@ const insertPhoto = async (req, res) => {
   const { title } = req.body;
   const image = req.file.filename;
 
-  console.log(req.body);
-  res.send("Foto inserida com sucesso");
+  const reqUser = req.user;
+  const user = await User.findById(reqUser._id);
+
+  // create a photo
+  const newPhoto = await Photo.create({
+    image,
+    title,
+    userId: user._id,
+    userName: user.name,
+  });
+
+  //if photo was created successfully, return data
+  if (!newPhoto) {
+    res
+      .status(422)
+      .json({ errors: ["Houve algum erro tente novamente mais tarde."] });
+  }
+
+  res.status(201).json(newPhoto);
 };
 
 module.exports = {
