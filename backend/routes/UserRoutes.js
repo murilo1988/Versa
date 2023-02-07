b32726a0de1026f72345d796c670b9205d719041
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-07 10:57:10 -0300

    created route to find user by id

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -7,6 +7,7 @@ const {
   login,
   getCurrentUser,
   update,
+  getUserById,
 } = require("../controllers/UserController");
 
 // middlewares
@@ -31,4 +32,5 @@ router.put(
   imageUpload.single("profileImage"),
   update
 );
+router.get("/:id", getUserById);
 module.exports = router;
