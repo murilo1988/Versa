cbd236712c80ac9516ada35f9334fb222400bb81
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-08 09:13:53 -0300

    was created the route to  get photo by id

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -8,6 +8,7 @@ const {
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
+  getPhotoById,
 } = require("../controllers/PhotoController");
 
 // middlewares
@@ -28,6 +29,7 @@ router.post(
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
 
 //
 module.exports = router;
