a583d9a49640976c4df50bfbcbd8d93248e42e9c
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-09 18:24:48 -0300

    redoing simpler css

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -7,21 +7,27 @@ import styles from "./NavBar.module.css";
 
 function NavBar() {
   return (
-    <nav className={styles.Navbar}>
-      <div className={styles.Navbar_brand}>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_brand}>
         <NavLink to="/">
           <h3>Versa</h3>
         </NavLink>
       </div>
-      <div className={styles.Navbar_links}>
+      <div className={styles.navbar_links}>
         <div>
-          <NavLink to="/">Home</NavLink>
+          <span>
+            <NavLink to="/">Home</NavLink>
+          </span>
         </div>
         <div>
-          <NavLink to="/login">Sign in</NavLink>
+          <span>
+            <NavLink to="/login">Sign in</NavLink>
+          </span>
         </div>
         <div>
-          <NavLink to="/register">Sign up</NavLink>
+          <span>
+            <NavLink to="/register">Sign up</NavLink>
+          </span>
         </div>
       </div>
     </nav>
