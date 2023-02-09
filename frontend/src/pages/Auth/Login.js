429f7f79e5e5490e562076b79bc7e5fa46def174
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-09 16:49:24 -0300

    page of home, login and register was created

---
+++ frontend/src/pages/Auth/Login.js
@@ -0,0 +1,7 @@
+import "./Auth.css";
+
+function Login() {
+  return <div>Login</div>;
+}
+
+export default Login;
---
+++ frontend/src/pages/Auth/Register.js
@@ -0,0 +1,7 @@
+import "./Auth.css";
+
+function Register() {
+  return <div>Register</div>;
+}
+
+export default Register;
