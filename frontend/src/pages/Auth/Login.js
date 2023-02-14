a15ed682a737b3bf190807b63be32e0115355cbe
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-14 14:25:29 -0300

    Created login and register pages  without CSS

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,7 +1,23 @@
 import './Auth.css'
 
+//Hooks
+import { useState, useEffect } from 'react'
+
 function Login() {
-  return <div>Login</div>
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+  return (
+    <div className='login_box'>
+      <p className='login_subtitle'>Entre para ver as fotos do seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type='email' placeholder='E-mail' />
+        <input type='password' placeholder='Senha' />
+
+        <input type='submit' value='Entrar' />
+      </form>
+    </div>
+  )
 }
 
 export default Login
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,31 @@
 import './Auth.css'
+// Components
+import { Link } from 'react-router-dom'
+
+//Hooks
+import { useState, useEffect } from 'react'
 
 function Register() {
-  return <div>Register</div>
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+  return (
+    <div className='register_box'>
+      <p className='register_subtitle'>
+        Cadastre-se para ver as fotos do seus amigos.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Nome' />
+        <input type='email' placeholder='E-mail' />
+        <input type='password' placeholder='Senha' />
+        <input type='password' placeholder='Confirme a senha' />
+        <input type='submit' value='Cadatrar' />
+        <p>
+          Já é cadastrado <Link to='/login'>clique aqui</Link>
+        </p>
+      </form>
+    </div>
+  )
 }
 
 export default Register
