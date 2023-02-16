d046ede69e956973f096d5db2b675491ecd7f47c
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-15 22:06:54 -0300

    starting with the functionalities of register

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,31 +1,65 @@
-import './Auth.css'
+import './Auth.css';
 // Components
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 //Hooks
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const handleSubmit = e => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+  };
+
   return (
     <div className='register_box'>
       <form className='form' onSubmit={handleSubmit}>
         <p className='subtitle_register'>
           Cadastre-se para ver as fotos do seus amigos.
         </p>
-        <input type='text' placeholder='Nome' />
-        <input type='email' placeholder='E-mail' />
-        <input type='password' placeholder='Senha' />
-        <input type='password' placeholder='Confirme a senha' />
-        <input type='submit' value='Cadatrar' />
+        <input
+          type='text'
+          placeholder='Nome'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type='email'
+          placeholder='E-mail'
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type='password'
+          placeholder='Senha'
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type='password'
+          placeholder='Confirme a senha'
+          onChange={e => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
+        <input type='submit' value='Cadastrar' onClick={handleSubmit} />
         <p className='go_to_login'>
-          Já é cadastrado <Link to='/login'>clique aqui</Link>
+          Já é cadastrado? <Link to='/login'>Clique aqui!</Link>
         </p>
       </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
