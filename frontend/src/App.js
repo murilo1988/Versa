455bda2bb54ace5e3cecccd426026549904c4a43
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-17 10:32:36 -0300

    Checking if the user is authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
-import './App.css'
+import "./App.css";
 
 //Router
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Hokks
+import { useAuth } from "./hooks/useAuth";
+
 // Components
-import NavBar from './components/NavBar/NavBar'
+import NavBar from "./components/NavBar/NavBar";
+
 // Pages
-import Home from './pages/Home/Home'
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
-import Footer from './components/Footer/Footer'
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  const { auth, loading } = useAuth();
+
+  console.log(loading);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
   return (
     <div className='App'>
       <BrowserRouter>
@@ -18,16 +30,25 @@ function App() {
         <div className='container'>
           <section className='section_left'></section>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route
+              path='/'
+              element={auth ? <Home /> : <Navigate to='/login' />}
+            />
+            <Route
+              path='/login'
+              element={!auth ? <Login /> : <Navigate to='/' />}
+            />
+            <Route
+              path='/register'
+              element={!auth ? <Register /> : <Navigate to='/' />}
+            />
           </Routes>
           <section className='section_right'></section>
         </div>
         <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
