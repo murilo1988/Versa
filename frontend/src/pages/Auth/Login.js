9962b50ca2d2033c0db3add0393f0553e2d3b556
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-22 21:16:39 -0300

    function login ok

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,24 +1,63 @@
-import './Auth.css'
+import "./Auth.css";
 
+//Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message/Message";
 //Hooks
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+//Redux
+import { login, reset } from "../../slices/authSlice";
 
 function Login() {
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(login(user));
+  };
+
+  //clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
   return (
     <div className='login_box'>
       <p className='subtitle_login'>Entre para ver as fotos do seus amigos.</p>
       <form className='form' onSubmit={handleSubmit}>
-        <input type='text' placeholder='Nome' />
-        <input type='email' placeholder='E-mail' />
+        <input
+          type='email'
+          placeholder='E-mail'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
 
-        <input type='submit' value='Entrar' />
+        {!loading && <input type='submit' value='Entrar' />}
+        {loading && <input type='submit' disabled value='Aguarde...' />}
+        {error && <Message msg={error} type='error' />}
       </form>
+      <p className='go_to_register'>
+        Não tem uma conta? <Link to='/register'>Clique aqui</Link>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
