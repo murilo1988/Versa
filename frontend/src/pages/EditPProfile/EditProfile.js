7c92eacda3f65d1fbe0883b92617182af03e4822
Murilo Cabreira <murilocabreira@hotmail.com>
2023-02-27 15:42:00 -0300

    making the css of the edit profile page

--- frontend/src/pages/EditPProfile/EditProfile.js
+++ frontend/src/pages/EditPProfile/EditProfile.js
@@ -11,25 +11,25 @@ function EditProfile() {
       <h2>Edite seus dados</h2>
       <p className='subtitle_profile'>
         Adicione uma imagem de perfil e conte mais sobre você
-        {/* preview da img */}
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='name' />
-          <input type='email' placeholder='E-mail' disable />
-          <label>
-            <span>Imagem do Perfil:</span>
-            <input type='file' />
-          </label>
-          <label>
-            <span>Bio:</span>
-            <input type='text' placeholder='Descrição do perfil.' />
-          </label>
-          <label>
-            <span>Quer alterar a senha</span>
-            <input type='password' placeholder='Digite uma nova senha.' />
-          </label>
-          <input type='submit' value='Atualizar' />
-        </form>
       </p>
+      {/* preview da img */}
+      <form className='form_profile' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Name' />
+        <input type='email' placeholder='E-mail' disable />
+        <label>
+          <span>Imagem do Perfil:</span>
+          <input type='file' />
+        </label>
+        <label>
+          <span>Bio:</span>
+          <input type='text' placeholder='Descrição do perfil.' />
+        </label>
+        <label>
+          <span>Quer alterar a senha:</span>
+          <input type='password' placeholder='Digite uma nova senha.' />
+        </label>
+        <input type='submit' value='Atualizar' />
+      </form>
     </div>
   );
 }
